0aaa2fe481eb9ad5686ad73272abe6d676897d90
IESTP VM <IESTP VM@DESKTOP-SLHAPHR>
2025-10-29 12:52:02 -0500

    carga inicial

---
+++ src/Controllers/SettingsControllers.java
@@ -0,0 +1,39 @@
+package Controllers;
+
+import Views.SystemViews;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class SettingsControllers implements MouseListener{
+    private SystemView views;
+    public SettingsControllers(SystemView views){
+    this.views = views;
+    
+        }
+            
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+  
+}
---
+++ src/Controllers/SystemView.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+/**
+ *
+ * @author IESTP VM
+ */
+class SystemView {
+
+    class jlabelProduct {
+
+        public jlabelProduct() {
+        }
+    }
+
+    class jLabelProducts {
+
+        static void addMouseListener(SettingsControllers aThis) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        public jLabelProducts() {
+        }
+    }
+    
+}
