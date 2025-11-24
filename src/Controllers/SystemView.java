2a44bffa51d90b956661f96d2a8464c7d06ee134
IESTP VM <IESTP VM@DESKTOP-SLHAPHR>
2025-11-24 12:44:35 -0500

    actualizacion de nuevo proyecto de pasteleria

---
+++ src/Controllers/SystemView.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+/**
+ *
+ * @author IESTP VM
+ */
+class SystemView {
+
+    class jlabelProduct {
+
+        public jlabelProduct() {
+        }
+    }
+
+    class jLabelProducts {
+
+        public jLabelProducts() {
+        }
+    }
+    
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Views.LoginView;
+
+public class Main {
+    public static void main(String[] args){
+       LoginView login = new LoginView();
+        login.setVisible(true);
+  }
+}
